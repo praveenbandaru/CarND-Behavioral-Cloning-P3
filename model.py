2dfb8b826bae19bd65bc54249dcea16c86af571d
Praveen Bandaru <praveen6.007@gmail.com>
2018-07-12 02:19:18 -0500

    Initial Upload. This project contains the working code for Behavioral Cloning using Python and Keras using Tensorflow backend. This project was developed as part of the 3rd project in Term 1 of Udacity's Self Driving Car Nano Degree.

---
+++ model.py
@@ -0,0 +1,93 @@
+import os
+import csv
+import cv2
+import numpy as np
+from PIL import Image
+import sklearn
+from random import shuffle
+
+import tensorflow as tf
+from keras.backend.tensorflow_backend import set_session
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True  # dynamically grow the memory used on the GPU
+config.log_device_placement = True  # to log device placement (on which device the operation ran)
+                                    # (nothing gets printed in Jupyter, only if you run it standalone)
+sess = tf.Session(config=config)
+set_session(sess)
+
+csv_file  = './data/driving_log.csv'
+path = './data/' # fill in the path to your training IMG directory
+
+samples = []
+with open(csv_file) as csvfile:
+    reader = csv.reader(csvfile)
+    headers = next(reader)
+    for line in reader:
+        samples.append(line)
+
+shuffle(samples)
+
+from sklearn.model_selection import train_test_split
+train_samples, validation_samples = train_test_split(samples, test_size=0.2) 
+
+def generator(samples, batch_size=32):
+    num_samples = len(samples)
+    while 1: # Loop forever so the generator never terminates
+        shuffle(samples)
+        for offset in range(0, num_samples, batch_size):
+            batch_samples = samples[offset:offset+batch_size]
+
+            images = []
+            angles = []
+            for batch_sample in batch_samples:
+                steering_center = float(batch_sample[3].strip())
+
+                # create adjusted steering measurements for the side camera images
+                correction = 0.2 # this is a parameter to tune
+                steering_left = steering_center + correction
+                steering_right = steering_center - correction
+        
+                # read in images from center, left and right cameras         
+                img_center = np.array(Image.open(path + batch_sample[0].strip()))
+                img_left = np.array(Image.open(path + batch_sample[1].strip()))
+                img_right = np.array(Image.open(path + batch_sample[2].strip()))
+        
+                # add images and angles to data set
+                images.extend((img_center, img_left, img_right, np.fliplr(img_center), np.fliplr(img_left), np.fliplr(img_right)))
+                angles.extend((steering_center, steering_left, steering_right, -steering_center, -steering_left, -steering_right))                
+
+            # trim image to only see section with road
+            X_train = np.array(images)
+            y_train = np.array(angles)
+            yield sklearn.utils.shuffle(X_train, y_train)
+
+
+
+# compile and train the model using the generator function
+train_generator = generator(train_samples, batch_size=32)
+validation_generator = generator(validation_samples, batch_size=32)
+
+from keras.models import Sequential
+from keras.layers import Flatten, Dense, Lambda, Cropping2D, Dropout
+from keras.layers.convolutional import Convolution2D
+from keras.layers.pooling import MaxPooling2D
+
+model = Sequential()
+model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160,320,3)))
+model.add(Cropping2D(cropping=((70,25), (0,0))))
+model.add(Convolution2D(24,5,5,subsample=(2,2),activation='relu'))
+model.add(Convolution2D(36,5,5,subsample=(2,2),activation='relu'))
+model.add(Convolution2D(48,5,5,subsample=(2,2),activation='relu'))
+model.add(Convolution2D(64,3,3,activation='relu'))
+model.add(Convolution2D(64,3,3,activation='relu'))
+model.add(Flatten())
+model.add(Dense(100))
+model.add(Dense(50))
+model.add(Dense(10))
+model.add(Dense(1))
+
+model.compile(optimizer='adam', loss='mse')
+model.fit_generator(train_generator, samples_per_epoch=len(train_samples)*6, validation_data=validation_generator, nb_val_samples=len(validation_samples)*6, nb_epoch=5)
+
+model.save('model.h5')
+
